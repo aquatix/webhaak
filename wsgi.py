fc015921c19766edd87f90fabea24a8c7c85da53
Michiel Scholten <michiel@diginaut.net>
2016-05-06 21:42:47 +0200

    Use a virtualenv from wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
+# Activate virtualenv
+import settings
+activate_this = settings.VENV
+execfile(activate_this, dict(__file__=activate_this))
+
 from webhaak import app as application
 
 if __name__ == "__main__":
