d4684fb40820dbaf05fca596fbc512cdc8a739bc
Michiel Scholten <michiel@diginaut.net>
2018-08-13 21:49:58 +0200

    PushOver example config

--- example_config/settings.py
+++ example_config/settings.py
@@ -15,3 +15,6 @@ LOG_LOCATION = 'webhaak.log'
 #LOG_LOCATION = '/var/log/webhaak/webhaak.log'
 # How many logs to keep in log rotation:
 LOG_BACKUP_COUNT = 10
+
+#PUSHOVER_USERKEY = '<fill_in_your_userkey>'
+#PUSHOVER_APPTOKEN = '<fill_in_the_apptoken_you_created>'
