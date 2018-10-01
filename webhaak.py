3cec9b6d164a2526c394bbfefd741d0be3c52929
Michiel Scholten <michiel@diginaut.net>
2018-10-01 15:27:25 +0200

    More and better logging; stdout before stderr is more logical

--- webhaak.py
+++ webhaak.py
@@ -168,6 +168,7 @@ def run_command(config):
         shell=True,
         universal_newlines=True
     )
+    logger.debug(result)
     return result
 
 
@@ -190,6 +191,7 @@ def do_pull_andor_command(config):
             notify_user(result, config)
 
     cmdresult = run_command(config)
+    logger.debug(cmdresult)
     if cmdresult.returncode == 0:
         logger.info('[%s] success for command: %s', projectname, str(cmdresult.stdout))
         result['status'] = 'OK'
@@ -204,8 +206,8 @@ def do_pull_andor_command(config):
             str(cmdresult.returncode),
             cmdresult.stderr
         )
-        logger.error('[%s] stderr: %s', projectname, cmdresult.stderr)
         logger.error('[%s] stdout: %s', projectname, cmdresult.stdout)
+        logger.error('[%s] stderr: %s', projectname, cmdresult.stderr)
 
     notify_user(result, config)
 
