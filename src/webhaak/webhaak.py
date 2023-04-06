bdb94607bccc30d4a521e8c1f6d75ac68176c953
Michiel Scholten <michiel@diginaut.net>
2023-04-06 12:07:02 +0200

    Structured project according to Python packaging guidelines

--- src/webhaak/webhaak.py
+++ src/webhaak/webhaak.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from redis import Redis
 from rq import Queue
 
-from core import incoming, tasks
+from src.webhaak import incoming, tasks
 
 app = FastAPI()
 DEBUG = os.getenv("DEBUG", "False")
